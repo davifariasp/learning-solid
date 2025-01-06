5187ee85c2971715ac642b36c97e354c79ef5238
davifariasp <davi.informatica8@gmail.com>
2025-01-06 15:22:23 -0300

    feature: adicionando os demais packages

---
+++ src/main/java/com/davifariasp/lsp/solucao/Main.java
@@ -0,0 +1,7 @@
+package com.davifariasp.lsp.solucao;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello wod!");
+    }
+}
---
+++ src/main/java/com/davifariasp/ocp/violacao/Main.java
@@ -0,0 +1,7 @@
+package com.davifariasp.ocp.violacao;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello wod!");
+    }
+}
